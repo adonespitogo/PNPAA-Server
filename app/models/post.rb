2604df0f64d12ccf824ca18379122ad04e594d3e
Adones <pitogo.adones@gmail.com>
2014-07-22 16:17:57 +0800

    Associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :department
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722080448) do
+ActiveRecord::Schema.define(version: 20140722081529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140722080448) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parent_id"
   end
 
   create_table "posts", force: true do |t|
